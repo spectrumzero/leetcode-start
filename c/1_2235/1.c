f5aa3b884d8dd79862009d7829649853f6b2194d
spectrumzero <lmhspecter@gmail.com>
2024-05-20 18:54:45 +0800

    第一个相加

---
+++ c/1_2235/1.c
@@ -0,0 +1,13 @@
+#include "../utils/common.h"
+
+int sum(int num1, int num2) { return num1 + num2; }
+
+int main() {
+  printf("请输入要相加的两个数字:\n");
+  int num1, num2;
+  scanf("%d,%d", &num1, &num2);
+  int s = sum(num1, num2);
+  printf("两数相加之和为：%d", s);
+
+  return 0;
+}
